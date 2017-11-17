44d44c8ae774a9f64262b513c96cd575c130fe48
viviansilvasc <viviansilvasc@gmail.com>
2017-11-17 19:49:03 -0200

    Add files via upload

---
+++ H2OMVC/H2OMVC/Controllers/H2OController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace H2OMVC.Controllers
+{
+    public class H2OController : Controller
+    { 
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Produto()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
